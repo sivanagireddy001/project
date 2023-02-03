3ae9044b90e601273f5a945c2256f51c8b12629d
sivanagireddy001 <120080943+sivanagireddy001@users.noreply.github.com>
2023-02-03 17:44:49 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,7 +1,18 @@
-// Your First Program in java in devops class
+import java.util.Scanner;
 
-class HelloWorld {
+public class HelloWorld {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+
+        // Creates a reader instance which takes
+        // input from standard input - keyboard
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+
+        // nextInt() reads the next integer from the keyboard
+        int number = reader.nextInt();
+
+        // println() prints the following line to the output screen
+        System.out.println("You entered: " + number);
     }
 }
